08be68b34092a723cd77053171b99d71409be3a9
Connor Cone <connor@sabrient.ai>
2021-01-28 15:24:13 -0600

    Env setup, initial message detection
    Signed-off-by: Connor Cone <connor@sabrient.ai>

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
 const Discord = require('discord.js');
-const client = new Discord.client();
+const client = new Discord.Client();
+const dotenv = require('dotenv');
+dotenv.config();
 
-client.on('read', () => {
-    console.log('Logged in as ${client.user.tag}!');
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user}!`);
 });
 
 client.on('message', msg => {
-    if (msg.content === 'ping') {
-        msg.channel.send('pong');
+    console.log('message received');
+    if (msg.author != client.user) {
+        if (msg.content === 'ping') {
+            msg.channel.send('pong');
+        } else {
+            console.log(msg.author);
+        }
     }
+        else {
+            // Bot is talking to itself
+            console.log('noticed a bot message!');
+        }
 });
 
-client.login('token');
+client.login(process.env.TOKEN);
+console.log(process.env.TOKEN);
